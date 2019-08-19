1f0a4f096d3f9b58d8410bafb5a673c69d3fc2d5
Le Duc <leduec@publicplan.de>
2019-08-19 23:01:05 +0200

    Check for required fields and submit

--- app/app.py
+++ app/app.py
@@ -8,5 +8,20 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/submit", methods=["POST"])
+def submit():
+    if request.method == "POST":
+        name = request.form["name"]
+        industry = request.form["industry"]
+        rating = request.form["rating"]
+
+        # print(name, industry, rating)
+
+        if name == "" or industry == "":
+            return render_template("index.html", message="Fill required fields!")
+
+        return render_template("success.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
